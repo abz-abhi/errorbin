da9823e2c3121e41d5a6facbefd8d49695db5984
abhishek <abhislinux@gmail.com>
2024-11-01 18:13:14 +0530

    select cion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
@@ -6,31 +6,35 @@ function App() {
       style={{ boxShadow: "4px 4px 40px #000 inset" }}
       className="h-screen w-screen bg-white"
     >
-      <div className="navbar flex justify-between items-center">
-        <img src="../public/Logo 1.png" alt="" className="pt-16 pl-10" />
-        <nav className=" pr-[38rem] ">
-          <div className="before-after-div bg-green-500 h-20 w-20 absolute "></div>
-          <ul className="flex gap-12 font-medium bg-pink-500">
-            <li>
-              <a href="home">HOME</a>
-            </li>
-            <li>
-              <a href="home">Website</a>
-            </li>
-            <li>
-              <a href="home">Application</a>
-            </li>
-            <li>
-              <a href="home">Digital Marketing</a>
-            </li>
-            <li>
-              <a href="home">Logos</a>
-            </li>
-            <li>
-              <a href="home">Posters</a>
-            </li>
-          </ul>
-        </nav>
+      <div className="navbar flex justify-between ">
+        <div>
+          <img src="../public/Logo 1.png" alt="" className="ml-4 mt-16" />
+        </div>
+        <div className=" mr-[40rem] ">
+          <div className="before-after-select h-16 w-20 absolute "></div>
+          <nav>
+            <ul className="flex gap-12 font-medium pt-4  w-full  ">
+              <li>
+                <a href="home">HOME</a>
+              </li>
+              <li>
+                <a href="home">Website</a>
+              </li>
+              <li>
+                <a href="home">Application</a>
+              </li>
+              <li>
+                <a href="home">Digital Marketing</a>
+              </li>
+              <li>
+                <a href="home">Logos</a>
+              </li>
+              <li>
+                <a href="home">Posters</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
